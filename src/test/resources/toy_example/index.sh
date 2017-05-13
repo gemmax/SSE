8545eae5a51fb8145c14c9d0c8e0303345abc5f2
gemmax <gemmax94@gmail.com>
2017-05-13 15:45:40 +0800

    20170513 undo spimi

--- src/test/resources/toy_example/index.sh
+++
@@ -1,2 +0,0 @@
-SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
-java -Xmx400M -cp $SCRIPTPATH/../../../../target/classes indexer.Index Gamma $1 $2
--- src/test/resources/toy_example/query.sh
+++
@@ -1,2 +0,0 @@
-SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
-java -Xmx200M -cp $SCRIPTPATH/../../../../target/classes querier.Query Gamma $1
